9663db4b69319ffe623ae25203e5bd838e4fb3ba
Tikila69 <didrik.sawkins@hotmail.com>
2024-03-11 14:47:54 +0100

    lagt inn vær API for å kunne få værvarsel for i dag og i morgen for hønefoss

--- stt.py
+++ stt.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
 from openai import OpenAI
+import weatherChecker
 
 def ask_openai(question):
     response = ""
@@ -31,26 +32,41 @@ while cont:
     with sr.Microphone() as source:
         print("Listening...")
         audio = r.listen(source)
-        try:
-            speech = r.recognize_google(audio)
-            if "hey jarvis" in speech.lower():
+        speech = r.recognize_google(audio)
+        if "hey jarvis" in speech.lower():
+            try:
                 engine.say("Yes, sir?")
+                print("Yes, sir?")
                 engine.runAndWait()
                 audio = r.listen(source)
-                try:
-                    speech = r.recognize_google(audio)
+                speech = r.recognize_google(audio)
+                if "shut down" in speech.lower():
+                    cont = False
+
+                elif "weather" in speech.lower():
                     print("You said: " + speech)
-                    response = ask_openai(speech)
-                    print("Response: ", response)
-                    engine.say(str(response))
-                    engine.runAndWait()
-                except sr.UnknownValueError:
-                    print("Google Speech Recognition could not understand audio")
-                except sr.RequestError as e:
-                    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-            elif "shut down" in speech.lower():
-                cont = False
-        except sr.UnknownValueError:
-            pass
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+                    try:
+                        responseToday = weatherChecker.weatherChecker("today")
+                        responseTomorrow = weatherChecker.weatherChecker("tomorrow")
+                        engine.say("today it will be "+str(responseToday[0])+" with a high of "+str(round((responseToday[1]-32)/1.8,1))+" and a low of "+str(round((responseToday[2]-32)/1.8, 1))+". Tomorrow it will be "+str(responseTomorrow[0])+" with a high of "+str(round((responseTomorrow[1]-32)/1.8,1))+" and a low of "+str(round((responseTomorrow[2]-32)/1.8, 1)))
+                        engine.runAndWait()
+                    except sr.UnknownValueError:
+                        print("Google Speech Recognition could not understand audio")
+                    except sr.RequestError as e:
+                        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+                else:
+                    try:
+                        speech = r.recognize_google(audio)
+                        print("You said: " + speech)
+                        #response = ask_openai(speech)
+                        print("Response: ", response)
+                        engine.say(str(response))
+                        engine.runAndWait()
+                    except sr.UnknownValueError:
+                        engine.say("I am sorry, I could not understand what you said")
+                    except sr.RequestError as e:
+                        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            except sr.UnknownValueError:
+                pass
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
