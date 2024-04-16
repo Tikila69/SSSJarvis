33401de42252bbbf4397e41433a124488aa3945e
Tikila69 <didrik.sawkins@hotmail.com>
2024-04-16 11:04:26 +0200

    fin

--- stt.py
+++ stt.py
@@ -57,59 +57,57 @@ turn_off(red_pin)
 cont = True
 
 
-try:
-    while cont:
-        with sr.Microphone(device_index=2) as source:
-            turn_off(red_pin)
-            turn_on(green_pin)
-            audio = r.listen(source)
-            speech = r.recognize_google(audio)
-            if "hey jarvis" in speech.lower():
-                try:
+
+while cont:
+    with sr.Microphone(device_index=2) as source:
+        turn_off(red_pin)
+        turn_on(green_pin)
+        audio = r.listen(source)
+        speech = r.recognize_google(audio)
+        if "hey jarvis" in speech.lower():
+            try:
+                turn_off(green_pin)
+                turn_on(red_pin)
+                engine.say("Yes, sir?")
+                turn_off(red_pin)
+                turn_on(green_pin)
+                engine.runAndWait()
+                audio = r.listen(source)
+                speech = r.recognize_google(audio)
+                if "shut down" in speech.lower():
                     turn_off(green_pin)
                     turn_on(red_pin)
-                    engine.say("Yes, sir?")
+                    engine.say("Shutting down")
+                    cont = False
+                    turn_off(green_pin)
                     turn_off(red_pin)
-                    turn_on(green_pin)
-                    engine.runAndWait()
-                    audio = r.listen(source)
-                    speech = r.recognize_google(audio)
-                    if "shut down" in speech.lower():
+                elif "weather" in speech.lower():
+                    turn_off(green_pin)
+                    turn_on(red_pin)
+                    print("You said: " + speech)
+                    try:
+                        responseToday = weatherChecker("today")
+                        responseTomorrow = weatherChecker("tomorrow")
+                        engine.say("today it will be "+str(responseToday[0])+" with a high of "+str(round((responseToday[1]-32)/1.8,1))+" and a low of "+str(round((responseToday[2]-32)/1.8, 1))+". Tomorrow it will be "+str(responseTomorrow[0])+" with a high of "+str(round((responseTomorrow[1]-32)/1.8,1))+" and a low of "+str(round((responseTomorrow[2]-32)/1.8, 1)))
+                        engine.runAndWait()
+                    except sr.UnknownValueError:
+                        print("Google Speech Recognition could not understand audio")
+                    except sr.RequestError as e:
+                        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+                else:
+                    try:
                         turn_off(green_pin)
                         turn_on(red_pin)
-                        engine.say("Shutting down")
-                        cont = False
-                        turn_off(green_pin)
-                        turn_off(red_pin)
-                    elif "weather" in speech.lower():
-                        turn_off(green_pin)
-                        turn_on(red_pin)
+                        speech = r.recognize_google(audio)
                         print("You said: " + speech)
-                        try:
-                            responseToday = weatherChecker.weatherChecker("today")
-                            responseTomorrow = weatherChecker.weatherChecker("tomorrow")
-                            engine.say("today it will be "+str(responseToday[0])+" with a high of "+str(round((responseToday[1]-32)/1.8,1))+" and a low of "+str(round((responseToday[2]-32)/1.8, 1))+". Tomorrow it will be "+str(responseTomorrow[0])+" with a high of "+str(round((responseTomorrow[1]-32)/1.8,1))+" and a low of "+str(round((responseTomorrow[2]-32)/1.8, 1)))
-                            engine.runAndWait()
-                        except sr.UnknownValueError:
-                            print("Google Speech Recognition could not understand audio")
-                        except sr.RequestError as e:
-                            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-                    else:
-                        try:
-                            turn_off(green_pin)
-                            turn_on(red_pin)
-                            speech = r.recognize_google(audio)
-                            print("You said: " + speech)
-                            #response = ask_openai(speech)
-                            engine.say(str(response))
-                            engine.runAndWait()
-                        except sr.UnknownValueError:
-                            engine.say("I am sorry, I could not understand what you said")
-                        except sr.RequestError as e:
-                            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-                except sr.UnknownValueError:
-                    pass
-                except sr.RequestError as e:
-                    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-except Exception as e:
-    pass
+                        #response = ask_openai(speech)
+                        engine.say(str(response))
+                        engine.runAndWait()
+                    except sr.UnknownValueError:
+                        engine.say("I am sorry, I could not understand what you said")
+                    except sr.RequestError as e:
+                        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
